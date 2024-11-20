7a3ea1f477b1ba512e1054bc573d447c971b933a
Tony Conway <tony.p.conway@gmail.com>
2024-11-20 16:43:41 +0000

    generate-csvs.js will always be run from directory

--- baseline/generate-csvs.js
+++ baseline/generate-csvs.js
@@ -3,8 +3,8 @@ import fs from "node:fs";
 
 console.log(process.cwd())
 
-fs.writeFileSync(process.cwd() + '/baseline/csv/core.csv', getBaselineCSV(), { flags: 'w' });
-fs.writeFileSync(process.cwd() + '/baseline/csv/core-with-downstream.csv', getBaselineCSV(true), { flags: 'w' });
-fs.writeFileSync(process.cwd() + '/baseline/csv/manifest.json', JSON.stringify({
+fs.writeFileSync('./csv/core.csv', getBaselineCSV(), { flags: 'w' });
+fs.writeFileSync('./csv/core-with-downstream.csv', getBaselineCSV(true), { flags: 'w' });
+fs.writeFileSync('./csv/manifest.json', JSON.stringify({
   lastUpdated: new Date()
 }), { flags: 'w' })
